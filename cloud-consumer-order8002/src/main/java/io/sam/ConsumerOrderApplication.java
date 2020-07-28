e43397d383d566aa0eba53eebc874d7fc4937eea
zhuweimu <635436613@qq.com>
2020-07-28 09:41:33 +0800

    添加测试接口

--- cloud-consumer-order8002/src/main/java/io/sam/ConsumerOrderApplication.java
+++ cloud-consumer-order8002/src/main/java/io/sam/ConsumerOrderApplication.java
@@ -1,15 +1,18 @@
 package io.sam;
 
+import io.sam.config.MySelfRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @Auther: zhuweimu
  * @Date: 2020/3/28 11:38
  * @Description:
  */
-@EnableEurekaClient
+//@EnableEurekaClient
+@RibbonClient(name = "cloud-provider-payment",configuration = {MySelfRuleConfig.class})
 @SpringBootApplication
 public class ConsumerOrderApplication {
 
