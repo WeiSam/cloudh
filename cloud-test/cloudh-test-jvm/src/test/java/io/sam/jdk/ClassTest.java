c0a32b53910cb0ff733e25f2c8b4319f3af34827
zhuweimu <18814383181@163.com>
2021-07-28 16:44:40 +0800

    add:测试jvm对象内存

--- cloud-test/cloudh-test-jvm/src/test/java/io/sam/jdk/ClassTest.java
+++ cloud-test/cloudh-test-jvm/src/test/java/io/sam/jdk/ClassTest.java
@@ -1,10 +1,14 @@
 package io.sam.jdk;
 
+import dto.vo.PaymentVo;
 import io.sam.dto.BaseDto;
 import io.sam.dto.FatherDto;
 import io.sam.dto.SonDto;
+import io.sam.dto.UserDto;
+import io.sam.oom.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.openjdk.jol.info.ClassLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +22,9 @@ public class ClassTest {
 
     Logger logger = LoggerFactory.getLogger(ClassTest.class);
 
+    private static Object obj = new Object();
+    private static UserDto userDto = new UserDto();
+
     @Test
     public void testAssignableFrom() {
         logger.info("{}",FatherDto.class.isAssignableFrom(SonDto.class));
@@ -26,6 +33,16 @@ public class ClassTest {
         logger.info("{}",SonDto.class.isAssignableFrom(BaseDto.class));
         if (new SonDto() instanceof FatherDto) {
 
+        }
+    }
+
+    @Test
+    public void testObject() {
+        System.out.println("对象信息");
+        System.out.println(ClassLayout.parseInstance(userDto).toPrintable());
+        System.out.println("==========对象信息加锁后变化============");
+        synchronized (userDto){
+            System.out.println(ClassLayout.parseInstance(userDto).toPrintable());
         }
     }
 }
