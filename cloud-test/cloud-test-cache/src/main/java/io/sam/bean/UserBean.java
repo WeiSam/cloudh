19aca927e9acaefe925012cac663b2f09c003c32
zhuweimu <635436613>
2021-12-11 10:31:01 +0800

    测试缓存

---
+++ cloud-test/cloud-test-cache/src/main/java/io/sam/bean/UserBean.java
@@ -0,0 +1,10 @@
+package io.sam.bean;
+
+/**
+ * @author zhuweimu
+ * @classname UserBean
+ * @description
+ * @date 2021/3/22 11:10
+ */
+public class UserBean {
+}
---
+++ cloud-test/cloud-test-cache/src/test/java/com/sam/InsertTest.java
@@ -0,0 +1,43 @@
+package com.sam;
+
+import io.sam.db.domain.TTest;
+import io.sam.db.mapper.TTestMapper;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author zhuweimu
+ * @desc
+ * @date 2021/8/29 21:31
+ */
+public class InsertTest extends BaseTest{
+
+    @Autowired
+    TTestMapper tTestMapper;
+
+    @Test
+    @Transactional
+    public void test01() throws InterruptedException {
+
+        int count = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(20);
+
+        for (int i=1;i<=count;i++){
+            int finalI = i;
+            new Thread(() -> {
+                TTest tTest = new TTest();
+                tTest.setName("sam"+ finalI);
+                tTest.setAge(0);
+                tTestMapper.insertSelective(tTest);
+                countDownLatch.countDown();
+            }).start();
+        }
+
+        countDownLatch.await();
+
+        System.out.println("执行结束");
+    }
+}
